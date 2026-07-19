9e50ba2ca9e7f0d2d03f7f83f3b8adf50d19f340
Bao Nguyen <bao.nguyen@u.nus.edu>
2026-07-19 20:20:30 +0800

    update init.lua

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -1,23 +1,23 @@
-vim.g.mapleader = " "
+vim.g.mapleader = ' '
 vim.g.have_nerd_font = true
 vim.o.number = true
-vim.o.mouse = "a"
+vim.o.mouse = 'a'
 vim.o.showmode = false
 vim.schedule(function()
-    vim.o.clipboard = "unnamedplus"
+    vim.o.clipboard = 'unnamedplus'
 end)
 vim.o.breakindent = true
 vim.o.undofile = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
-vim.o.signcolumn = "yes"
+vim.o.signcolumn = 'yes'
 vim.o.updatetime = 250
 vim.o.timeoutlen = 300
 vim.o.splitright = true
 vim.o.splitbelow = true
 vim.o.list = true
-vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
-vim.o.inccommand = "split"
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.o.inccommand = 'split'
 vim.o.cursorline = true
 vim.o.scrolloff = 10
 vim.o.confirm = true
@@ -32,7 +32,7 @@ vim.opt.shiftwidth = 4
 vim.diagnostic.config({
     update_in_insert = false,
     severity_sort = true,
-    float = { border = "rounded", source = "if_many" },
+    float = { border = 'rounded', source = 'if_many' },
     underline = { severity = vim.diagnostic.severity.ERROR },
 
     virtual_text = true,
@@ -40,69 +40,69 @@ vim.diagnostic.config({
     jump = { float = true },
 })
 
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
-vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
-vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
-vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
-vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
-vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
-vim.keymap.set("n", "<A-h>", ":tabprevious<CR>", { desc = "Go to previous tab" })
-vim.keymap.set("n", "<A-l>", ":tabnext<CR>", { desc = "Go to next tab" })
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set('n', '<A-h>', ':tabprevious<CR>', { desc = 'Go to previous tab' })
+vim.keymap.set('n', '<A-l>', ':tabnext<CR>', { desc = 'Go to next tab' })
 
-vim.keymap.set("i", "{", "{}<Left>", { noremap = true, silent = true })
-vim.keymap.set("i", "<CR>", function()
-    local line = vim.fn.getline(".")
-    local col = vim.fn.col(".") - 1
+vim.keymap.set('i', '{', '{}<Left>', { noremap = true, silent = true })
+vim.keymap.set('i', '<CR>', function()
+    local line = vim.fn.getline('.')
+    local col = vim.fn.col('.') - 1
 
-    if col >= 1 and col < #line and line:sub(col, col + 1) == "{}" then
-        return vim.api.nvim_replace_termcodes("<CR><Esc>O", true, false, true)
+    if col >= 1 and col < #line and line:sub(col, col + 1) == '{}' then
+        return vim.api.nvim_replace_termcodes('<CR><Esc>O', true, false, true)
     end
 
-    return vim.api.nvim_replace_termcodes("<CR>", true, false, true)
+    return vim.api.nvim_replace_termcodes('<CR>', true, false, true)
 end, { expr = true, noremap = true, silent = true })
 
-vim.api.nvim_create_autocmd("TextYankPost", {
-    desc = "Highlight when yanking (copying) text",
-    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'Highlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
     callback = function()
         vim.hl.on_yank()
     end,
 })
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+    local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
     if vim.v.shell_error ~= 0 then
-        error("Error cloning lazy.nvim:\n" .. out)
+        error('Error cloning lazy.nvim:\n' .. out)
     end
 end
 
 local rtp = vim.opt.rtp
 rtp:prepend(lazypath)
 
-require("lazy").setup({
-    { import = "plugins" },
+require('lazy').setup({
+    { import = 'plugins' },
 }, {
     git = {
         timeout = 300,
     },
     ui = {
         icons = vim.g.have_nerd_font and {} or {
-            cmd = "⌘",
-            config = "🛠",
-            event = "📅",
-            ft = "📂",
-            init = "⚙",
-            keys = "🗝",
-            plugin = "🔌",
-            runtime = "💻",
-            require = "🌙",
-            source = "📄",
-            start = "🚀",
-            task = "📌",
-            lazy = "💤 ",
+            cmd = '⌘',
+            config = '🛠',
+            event = '📅',
+            ft = '📂',
+            init = '⚙',
+            keys = '🗝',
+            plugin = '🔌',
+            runtime = '💻',
+            require = '🌙',
+            source = '📄',
+            start = '🚀',
+            task = '📌',
+            lazy = '💤 ',
         },
     },
 })
